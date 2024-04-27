70437fbf6d93bf219bc068ffb455b748d3a25085
Semandr <semandr.code@gmail.com>
2024-04-27 22:02:57 +0200

    add control to backdrop and Cancel btn

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,19 @@
 'use strict';
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieBtn = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieBtn = addMovieModal.querySelector('.btn--passive');
 
-function toggleMovieModal() {
+const toggleBackdrop = () => backdrop.classList.toggle('visible');
+
+const toggleMovieModal = () => {
   addMovieModal.classList.toggle('visible');
-}
+  toggleBackdrop();
+};
+
+const backdropClickHandler = () => toggleBackdrop();
+const cancelAddMovie = () => toggleMovieModal();
 
 startAddMovieBtn.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
+cancelAddMovieBtn.addEventListener('click', cancelAddMovie);
