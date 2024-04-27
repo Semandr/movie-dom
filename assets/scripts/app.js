5224ce0fbbcdd2a3b33a5b314c086e4b6a5d73a3
Semandr <semandr.code@gmail.com>
2024-04-27 21:45:21 +0200

    Opening a modal

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,9 @@
 'use strict';
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieBtn = document.querySelector('header button');
+
+function toggleMovieModal() {
+  addMovieModal.classList.toggle('visible');
+}
+
+startAddMovieBtn.addEventListener('click', toggleMovieModal);
