aa17de4947fa12fa82670ed1d19759b6d3535426
asemenchenko <semandr.code@gmail.com>
2024-04-30 21:36:41 +0200

    Creating a movie in JS and clear user inputs

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,16 +6,28 @@ const cancelAddMovieBtn = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieBtn = cancelAddMovieBtn.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
 
-const toggleBackdrop = () => backdrop.classList.toggle('visible');
+const movies = [];
 
-const toggleMovieModal = () => {
+function toggleBackdrop() {
+  backdrop.classList.toggle('visible');
+}
+
+function toggleMovieModal() {
   addMovieModal.classList.toggle('visible');
   toggleBackdrop();
-};
+}
 
-const backdropClickHandler = () => toggleBackdrop();
+function backdropClickHandler() {
+  toggleBackdrop();
+}
 
-const addMovieHandler = () => {
+function clearMovieInputs() {
+  for (const userInput of userInputs) {
+    userInput.value = '';
+  }
+}
+
+function addMovieHandler() {
   const titleValue = userInputs[0].value;
   const imageUrlValue = userInputs[1].value;
   const ratingValue = userInputs[2].value;
@@ -30,8 +42,21 @@ const addMovieHandler = () => {
     alert('Please enter valid values');
     return;
   }
-};
-const cancelAddMovieHandler = () => toggleMovieModal();
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInputs();
+}
+
+function cancelAddMovieHandler() {
+  toggleMovieModal();
+  clearMovieInputs();
+}
 
 startAddMovieBtn.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
